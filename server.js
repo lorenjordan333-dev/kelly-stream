88f6e751041fa221b20530017f8c79e8691563a7
lorenjordan333-dev <lorenjordan333@gmail.com>
2026-04-04 20:27:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ Only if the customer asks how long, say:
             type: "server_vad",
             threshold: 0.6,
             prefix_padding_ms: 500,
-            silence_duration_ms: 850,
+            silence_duration_ms: 750,
           },
         },
       })
