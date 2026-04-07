b5f5d5c5b0644176b237b35f0254a40308eda107
lorenjordan333-dev <lorenjordan333@gmail.com>
2026-04-07 21:59:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -360,7 +360,7 @@ wssTwilio.on("connection", (ws) => {
           type: "server_vad",
           threshold: 0.5,
           prefix_padding_ms: 300,
-          silence_duration_ms: 900,
+          silence_duration_ms: 400,
         },
       },
     }));
@@ -539,7 +539,7 @@ wssWeb.on("connection", (ws) => {
           type: "server_vad",
           threshold: 0.5,
           prefix_padding_ms: 300,
-          silence_duration_ms: 900,
+          silence_duration_ms: 400,
         },
       },
     }));
